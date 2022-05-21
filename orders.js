3c9a7f1eba741f8833cada63d527c7c5458148f3
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-05-21 05:46:32 +0100

    Finalized Project

---
+++ orders.js
@@ -0,0 +1,47 @@
+const Orders = [
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85372',
+    paymentStatus: 'Due',
+    shipping: 'Pending' 
+    },
+
+    {
+    productName: 'GPS 4k Drone',
+    productNumber: '78230',
+    productStatus: 'Due',
+    shipping: 'Declined' 
+    },
+    {
+    productName: 'MX master 3',
+    productNumber: '23786',
+    productStatus: 'Due',
+    shipping: 'Pending' 
+    },
+    {
+    productName: 'Ruko F11 Drone',
+    productNumber: '23473',
+    productStatus: 'Due',
+    shipping: 'Delivered' 
+    },
+    {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '98747',
+    productStatus: 'Due',
+    shipping: 'Delivered' 
+    },
+    {
+    productName: 'Lozenge Drone',
+    productNumber: '34658',
+    productStatus: 'Due',
+    shipping: 'Pending' 
+    },
+    {
+    productName: 'DJI Air 25',
+    productNumber: '85019',
+    productStatus: 'Due',
+    shipping: 'Delivered' 
+    }
+
+   
+]
